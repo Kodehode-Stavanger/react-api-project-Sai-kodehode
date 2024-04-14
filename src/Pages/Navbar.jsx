b94d6865914d1c9b9b522bc07013f3d150217875
Sai Kodehode <sai.kodehode@gmail.com>
2024-04-14 23:50:37 +0200

    updated searchbar

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,4 +1,4 @@
-// Navbar.js
+// src/components/Navbar.js
 import React, { useState } from "react";
 import axios from "axios";
 import Spinner from "../Components/Spinner";
@@ -8,25 +8,28 @@ const Navbar = ({ onSearch }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async () => {
-    if (keyword.trim() !== "") {
-      try {
-        setIsLoading(true);
-        const response = await axios.get(
-          `https://v1.nocodeapi.com/sai12/spotify/tFzVqPcQbFzBNLGS/search?q=${keyword}&type=track`
-        );
-        const tracks = response.data.tracks.items;
-        onSearch(tracks);
-      } catch (error) {
-        console.error("Error fetching tracks:", error);
-      } finally {
-        setIsLoading(false);
-      }
+    console.log("Searching with keyword:", keyword);
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `https://v1.nocodeapi.com/sai12/spotify/tFzVqPcQbFzBNLGS/search?q=${
+          keyword === "" ? "trending" : keyword
+        }&type=track`
+      );
+      console.log("Response:", response.data);
+      const tracksData = response.data.tracks.items;
+      onSearch(tracksData);
+    } catch (error) {
+      console.error("Error fetching tracks:", error);
+      onSearch([]); // Notify parent of error
+    } finally {
+      setIsLoading(false);
     }
   };
-
   return (
     <nav className="navbar">
       <div className="search-container">
+        <p>Spotify</p>
         <input
           value={keyword}
           onChange={(event) => setKeyword(event.target.value)}
