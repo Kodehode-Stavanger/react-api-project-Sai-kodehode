fa8facadc55e7a4e503c1fa204f52ef53372a490
Sai Kodehode <sai.kodehode@gmail.com>
2024-04-16 00:32:36 +0200

    Added styling to HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import SearchBar from "./SearchBar";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   const { keyword } = useParams();
@@ -32,25 +33,27 @@ const HomePage = () => {
   }, [keyword]);
 
   return (
-    <div className="home-page">
+    <div className={styles.homePage}>
       <SearchBar onSearch={(data) => setTracks(data)} />
-      <div className="track-list">
+      <div className={styles.trackList}>
         {isLoading ? (
-          <p>Loading...</p>
+          <p className={styles.loading}>Loading...</p>
         ) : (
           tracks.map((track) => (
-            <div key={track.id} className="track-item">
+            <div key={track.id} className={styles.trackItem}>
               <div>
                 <img src={track.album.images[0].url} alt={track.name} />
               </div>
               <h3>{track.name}</h3>
-              <p>Artist: {track.artists[0].name}</p>
-              <p>Popularity: {track.popularity}</p>
-              <p>Release Date: {track.album.release_date}</p>
-              <audio controls>
-                <source src={track.preview_url} type="audio/mp3" />
-                Your browser does not support the audio element.
-              </audio>
+              <div className={styles.trackDetails}>
+                <p>Artist: {track.artists[0].name}</p>
+                <p>Popularity: {track.popularity}</p>
+                <p>Release Date: {track.album.release_date}</p>
+                <audio controls>
+                  <source src={track.preview_url} type="audio/mp3" />
+                  Your browser does not support the audio element.
+                </audio>
+              </div>
             </div>
           ))
         )}
--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./NotFoundPage.module.css";
 export default function NotFoundPage({ msg }) {
+  const handleLinkClick = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className={styles.notFoundContainer}>
       <h1 className={styles.notFoundHeading}>{msg}</h1>
-      <Link className={styles.homeLink} to="/search">
+      <Link className={styles.homeLink} to="/search" onClick={handleLinkClick}>
         Go to Home
       </Link>
     </div>
